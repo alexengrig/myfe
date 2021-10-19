a150b8596d0c50c06859b02caefc27aca1a96dd7
Grig Alex <alexengrigdev@gmail.com>
2021-10-19 04:50:27 +0300

    Create PathUtil

---
+++ src/main/java/dev/alexengrig/myfe/util/PathUtil.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2021 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.myfe.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * A utility class for {@link Path}.
+ */
+public final class PathUtil {
+
+    private PathUtil() throws IllegalAccessException {
+        throw new IllegalAccessException("This is utility class");
+    }
+
+    public static String getName(Path path) {
+        if (requireNonNullPath(path).getNameCount() == 0) {
+            return path.toString();
+        }
+        return path.getFileName().toString();
+    }
+
+    public static String getAbsolutePath(Path path) {
+        return requireNonNullPath(path).toAbsolutePath().toString();
+    }
+
+    public static boolean isDirectory(Path path) {
+        return doIsDirectory(requireNonNullPath(path));
+    }
+
+    private static boolean doIsDirectory(Path path) {
+        return Files.isDirectory(path);
+    }
+
+    public static boolean nonDirectory(Path path) {
+        return doNonDirectory(requireNonNullPath(path));
+    }
+
+    private static boolean doNonDirectory(Path path) {
+        return !doIsDirectory(path);
+    }
+
+    public static List<Path> getSubdirectories(Path path) {
+        requireDirectory(requireNonNullPath(path));
+        try (Stream<Path> stream = Files.list(path)) {
+            return stream.filter(PathUtil::doIsDirectory).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalArgumentException("For path: " + path, e);
+        }
+    }
+
+    public static List<Path> getChildren(Path path) {
+        requireDirectory(requireNonNullPath(path));
+        try (Stream<Path> stream = Files.list(path)) {
+            return stream.collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalArgumentException("For path: " + path, e);
+        }
+    }
+
+    private static Path requireNonNullPath(Path path) {
+        return requireNonNull(path, "The path must not be null");
+    }
+
+    private static void requireDirectory(Path path) {
+        if (doNonDirectory(path)) {
+            throw new IllegalArgumentException("The path isn't directory: " + path);
+        }
+    }
+
+}
