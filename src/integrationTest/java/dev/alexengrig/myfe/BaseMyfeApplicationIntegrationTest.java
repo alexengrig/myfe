4ec33beef2280d022c3b72d2c2f72d19131d916e
Grig Alex <alexengrigdev@gmail.com>
2021-12-09 00:45:12 +0300

    Create BaseMyfeApplicationIntegrationTest

---
+++ src/integrationTest/java/dev/alexengrig/myfe/BaseMyfeApplicationIntegrationTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.myfe;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.edt.GuiQuery;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.testng.testcase.AssertJSwingTestngTestCase;
+
+public abstract class BaseMyfeApplicationIntegrationTest extends AssertJSwingTestngTestCase {
+
+    private FrameFixture window;
+
+    protected FrameFixture window() {
+        return window;
+    }
+
+    @Override
+    protected final void onSetUp() {
+        MyfeApplication mainWindow = createMyfeApplication();
+        this.window = new FrameFixture(this.robot(), mainWindow);
+        this.window.show();
+    }
+
+    private MyfeApplication createMyfeApplication() {
+        return GuiActionRunner.execute(new GuiQuery<>() {
+
+            @Override
+            protected MyfeApplication executeInEDT() {
+                return new MyfeApplication();
+            }
+
+        });
+    }
+
+    @Override
+    protected void onTearDown() {
+        super.onTearDown();
+        this.window = null;
+    }
+
+}
